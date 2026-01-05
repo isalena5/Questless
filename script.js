fbe85467e441ab0e7659823414144f50219db79d
isalena5 <isabelgarc2003@gmail.com>
2026-01-04 21:10:32 -0600

    Update v0.0.0 - Changed to DaisyUI

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ const listContainer = document.getElementById("list-container");
 function addTask(){
     if(inputBox.value == ''){
         alert("You must write something!");
+        return;
     }
 
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value="";
+    const li = document.createElement("li");
+    li.className = "flex items-center gap-3"
+        
+    li.innerHTML = `<input type="checkbox" class="checkbox checkbox-primary" />
+    <span class="flex-1">${inputBox.value}</span>
+    <button class="btn btn-sm btn-ghost text-error>X</button>
+    `;
+
+    listContainer.appendChild(li);
+
+    inputBox.value = "";
     saveData();
 }
 
 listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
+    
+    // Checkbox Toggled
+    if(e.target.type === "checkbox"){
+        e.target.nextElementSibling.classList.toggle("line-through");
         saveData();
     }
-    else if (e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
+
+    // Delete Task
+    else if (e.target.closest("button")){
+        e.target.closest().remove();
         saveData();
     }
 }, false);
@@ -35,6 +42,11 @@ function saveData(){
 
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");    
+}
+
+function deleteAll(){
+    localStorage.removeItem("data");
+    location.reload();
 }
 
 showTask();
